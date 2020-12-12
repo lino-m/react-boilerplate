6e399b841feb22355bc268ae346ac5f9476fb87e
Lino Maurer <lino@q-b.de>
2020-12-12 13:01:27 +0100

    new map, new state: selectedFlat, new function: set

--- src/components/FlatsList.jsx
+++ src/components/FlatsList.jsx
@@ -4,11 +4,12 @@ import FlatCard from "./FlatCard";
 // eslint-disable-next-line react/prefer-stateless-function
 class FlatsList extends React.Component {
   render() {
-    const { flats } = this.props;
+    const { flats, selectFlat } = this.props;
     const list = [];
     flats.map((flat) => {
-      console.log(flat);
-      list.push(<FlatCard flat={flat} key={flat.name} />);
+      list.push(
+        <FlatCard flat={flat} key={flat.name} selectFlat={selectFlat} />
+      );
     });
 
     return <div className="flat-list">{list}</div>;
